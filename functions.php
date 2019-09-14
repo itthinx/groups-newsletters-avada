329d1efea73ebab21b4b9aa78a34168419b481e0
itthinx <itthinx@gmail.com>
2019-09-14 13:42:35 +0200

    corrected function names to use theme-specific prefixes and names according to the hook

--- functions.php
+++ functions.php
@@ -16,13 +16,19 @@ if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function theme_enqueue_styles() {
+/**
+ * Enqueue styles for the theme.
+ */
+function groups_newsletters_avada_wp_enqueue_scripts() {
 	wp_enqueue_style( 'groups-newsletters-avada', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'groups_newsletters_avada_wp_enqueue_scripts' );
 
-function avada_lang_setup() {
+/**
+ * Loads the child theme's translations.
+ */
+function groups_newsletters_avada_after_setup_theme() {
 	$lang = get_stylesheet_directory() . '/languages';
-	load_child_theme_textdomain( 'Avada', $lang );
+	load_child_theme_textdomain( 'groups-newsletters-avada', $lang );
 }
-add_action( 'after_setup_theme', 'avada_lang_setup' );
+add_action( 'after_setup_theme', 'groups_newsletters_avada_after_setup_theme' );
